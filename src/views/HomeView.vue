9aa9fa60f1b2901951c41319db39dc7e3e84122c
SpiegelV <leon.schuessler@freenet.de>
2022-06-08 03:59:59 +0200

    "Hello World" completed.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Mate-Finder, your place for rating and finding Mate-Softdrinks."/>
   </div>
 </template>
 
