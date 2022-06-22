a060fe9740bc132ef2eac2b8fca439637ee0a213
SpiegelV <leon.schuessler@freenet.de>
2022-06-22 03:17:12 +0200

    Added Mate Cards to MateView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>It is 4:26am and I am tired</h1>
+    <h1>Mate-Finder: A University project for the Business Computing 3rd semester-course "Web Technologies".
+      Project developed by: Lucas Leiva (MNr. 579799) and Leon Schüßler (MNr. 579906).
+      A small Web-Application for students to rate different brands and flavours of Mate-Softdrinks and save those ratings online.
+      The plan is to later on add the ability to search for, and find those kinds of drinks via a Maps API.</h1>
   </div>
 </template>
--- src/views/MateView.vue
+++ src/views/MateView.vue
@@ -1,5 +1,20 @@
 <template>
-<h1>Here are the Mate Drinks we know.</h1>
+  <div>
+    <h1>Here are the Mate Drinks we know.</h1>
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col" v-for="mate in mates" :key="mate.id">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{ mate.name }}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Preis: {{ mate.price }}</h6>
+              <p class="card-text">User Rating: Mate/10</p>
+              <a href="#" class="card-link">Add to Favourites</a>
+              <a href="#" class="card-link">Rate this Mate</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
