694f1cd11a59c4a2ac65d1e89b044c5d79673293
SpiegelV <leon.schuessler@freenet.de>
2022-07-03 17:32:58 +0200

    Fixing CORS for Finder (hopefully)

--- src/main.js
+++ src/main.js
@@ -5,15 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 import Vue3Geolocation from 'vue3-geolocation'
 import VueGoogleMaps from '@fawmi/vue-google-maps'
 
-library.add(fas)
+library.add(faStar)
 
 createApp(App)
   .component('fa', FontAwesomeIcon)
   .use(Vue3Geolocation)
-  .use(VueGoogleMaps, { load: { key: 'AIzaSyCjJgSzZu0QC8QFrfSfbLZhqJPcclz9xlI' } })
+  .use(VueGoogleMaps, {
+    load: {
+      libraries: 'places',
+      key: 'AIzaSyCjJgSzZu0QC8QFrfSfbLZhqJPcclz9xlI'
+    }
+  })
   .use(router)
   .mount('#app')
